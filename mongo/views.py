eb98f231a5271cc1ef117d0ac7c906bba8dfa82e
bianXiaoTuan <chenhuan0102@126.com>
2015-11-24 11:41:19 +0800

    更新views获取数据接口

--- mongo/views.py
+++ mongo/views.py
@@ -15,7 +15,7 @@ def visit_record(request):
     """展示用户访问报表
     """
     # 测试用数据
-    # ip_info = mongo.mock_data.mock_ip_info()
+    # location_info = mongo.mock_data.mock_location_info()
     # day_visit_info = mongo.mock_data.mock_day_visit_info()
     # os_info = mongo.mock_data.mock_os_info()
     # browser_info = mongo.mock_data.mock_browser_info()
@@ -23,18 +23,13 @@ def visit_record(request):
     # total_visit_num = mongo.mock_data.mock_total_visit_num()
 
     # 线上数据
-    city_info = mongo.mongo_engine.get_city_info()
-    day_visit_info = mongo.mongo_engine.get_day_visit_info()
-    os_info = mongo.mongo_engine.get_os_info()
-    browser_info = mongo.mongo_engine.get_browser_info()
-    url_path_info = mongo.mongo_engine.get_url_path_info()
-    total_visit_num = mongo.mongo_engine.get_total_visit_num()
+    access_info = mongo.mongo_engine.get_access_info()
 
     return render_to_response('visit_record.html', {
-        'city_info': city_info,
-        'os_info': os_info,
-        'browser_info': browser_info,
-        'url_path_info': url_path_info,
-        'day_visit_info': day_visit_info,
-        'total_visit_num': total_visit_num,
+        'location_info': json.dumps(access_info['location_info']),
+        'os_info': json.dumps(access_info['os_info']),
+        'browser_info': json.dumps(access_info['browser_info']),
+        'url_path_info': json.dumps(access_info['url_path_info']),
+        'day_visit_info': json.dumps(access_info['day_visit_info']),
+        'total_visit_num': access_info['total_visit_num'],
         } )
